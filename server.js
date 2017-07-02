1ac870ab0545bd1cb9bea54c6adc6c037afac2b1
Alex I. Ramirez <alex@arcompware.com>
2017-07-01 22:59:07 -0700

    Start message changed

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ var server = app.listen(process.env.PORT || 8080, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log("API listening at http://%s:%s", host, port)
+    console.log("Hall of Fame listening at http://%s:%s", host, port)
 });
