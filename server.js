f0205e6c31cb85e65ce212e0a5e3575b502d9128
Alex I. Ramirez <alex@arcompware.com>
2017-07-02 17:34:42 -0700

    Initial bootstrap setup

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 var express = require('express');
+var engine = require('ejs-locals');
 var app     = express();
 
 var api     = require('./api');
 
+app.engine('ejs', engine);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.set('view options', { layout:'views/layouts/layout.ejs' });
+
+
+app.use('/public', express.static('public'));
+
+process.on('uncaughtException', function (err) {
+  console.log("Error: " + err);
+});
+
 
 app.use('/api', api);
 
-app.get('/', function(req, res){
-    res.status(200).json({message:'Connected to Hall of Fame!'});
+app.get('/', function (req, res) {
+  var title = "";
+  var content = "<p>Instructions to come.</p>";
+  res.render('pages/stdpage', {
+    "title": title,
+    "content": content
+  });
 });
+
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var host = server.address().address
